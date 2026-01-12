d0bda913682fb9486e158c14fc97b2f149686e3f
Steve Williamson <steve@sjdub.com>
2026-01-12 10:46:02 -0500

    Remove border top from footer completely

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="filament-footer w-full text-center py-4 border-t border-gray-200 dark:border-gray-700">
+<div class="filament-footer w-full text-center py-4">
     <p class="text-sm text-gray-600 dark:text-gray-400">
         {{ config('app.name', 'Portal') }} is provided by <a href="https://tappnetwork.com" target="_blank" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 underline">Tapp Network</a>. © 2026 Tapp Network. All rights reserved.
     </p>
