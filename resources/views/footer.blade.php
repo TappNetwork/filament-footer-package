c67d3493577310e924fcc830dddf2a2bdf4d0ee3
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2026-01-13 06:35:57 -0300

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="filament-footer w-full text-center py-4">
     <p class="text-sm text-gray-600 dark:text-gray-400">
-        {{ config('app.name', 'Portal') }} is provided by <a href="https://tappnetwork.com" target="_blank" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 underline">Tapp Network</a>. © 2026 Tapp Network. All rights reserved.
+        {{ config('app.name', 'Portal') }} is provided by <a href="https://tappnetwork.com" target="_blank" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 underline">Tapp Network</a>. © {{ date("Y") }} Tapp Network. All rights reserved.
     </p>
 </div>
